e9a42ecf0ecfce3bf04593504f2a78155838c69d
SamyakBorkar <2022bit037@sggs.ac.in>
2024-08-10 23:36:47 +0530

    Cudtom Modulo Function made

---
+++ Practical dir/Practical 3/modulus.py
@@ -0,0 +1,32 @@
+'''
+THIS CODE IS MADE BY SAMYAK BORKAR 
+THIS IS A CUSTOM MODULO FUNCTION CODE WHICH HANDELS CASESLIKE:
+1.WHEN DENOMINATOR IS EQUL TO NUMERATOR
+2.WHEN DENOMINATOR IS EQUAL TO ZERO AND NUMERATOR IS INTEGER
+3.WHEN DENOMINATOR IS EQUAL TO ZERO AND NUMERATOR IS FLOAT
+4.WHEN DENOMINATOR IS GREATOR THAN NUMERATOR
+5.AND THEN LAST IS FOR HANDELING MODULO FOR ALL NUMBERS IN WHICH I HAVE IMPLEMENTED THE LOGIC OF :
+QUOTIENTT * DENOMINATOR + REMAINDER = NUMERATOR 
+'''
+
+def moduloFunc(numerator, denominator):
+    if numerator == denominator:
+        return 0
+    elif denominator == 0 and isinstance(numerator, int):
+        return "ZeroDivisionError: integer modulo by zero"
+    elif denominator == 0 and isinstance(numerator, float):
+        return "ZeroDivisionError: float modulo"
+    elif numerator < denominator:
+        return numerator
+    elif isinstance(numerator, int) and isinstance(denominator, int):
+        quotient = numerator // denominator
+        remainder = numerator - (denominator * quotient)
+        return remainder
+    elif not (isinstance(numerator, int) and isinstance(denominator, int)):
+        quotient = numerator / denominator 
+        remainder = numerator - (denominator * int(quotient))
+        return remainder
+    else:
+        return "ERROR 404"
+
+print(moduloFunc(7.4, 5.0))
